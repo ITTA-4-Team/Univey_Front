90c18fbbf748cfca1a58efb627a9569fa96452b2
sangyeon <asylee02@gmail.com>
2024-01-20 23:13:53 +0900

    수정

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customaxios = axios.create({
-  baseURL: "https://www.univey.site",
+  baseURL: "https://univey.site",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "69420",
